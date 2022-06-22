cfdb0ebb83ad3847b7b8d9c1a334fb38fa98c10e
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-22 12:15:54 -0400

    add order model

--- api/models/order.py
+++ api/models/order.py
@@ -1,6 +1,9 @@
 from api.models.db import db 
+from datetime import datetime
 
 class Order(db.Model):
   __tablename__ = 'orders'
   id = db.Column(db.Integer, primary_key=True)
-  
+  profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+  cost = db.Column(db.Float)
