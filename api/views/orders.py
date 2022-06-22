83a5dadb2c18ac1722b63799ffb47b3b2a078179
gkatsikis <geo.bjj.kat@gmail.com>
2022-06-22 13:59:15 -0400

    add create function in order view

--- api/views/orders.py
+++ api/views/orders.py
@@ -6,3 +6,13 @@ from api.models.order import Order
 
 orders = Blueprint('orders', 'orders')
 
+@orders.route('/', methods=["POST"])
+@login_required
+def create():
+  data = request.get_json()
+  profile = read_token(request)
+  data["profile_id"] = profile["id"]
+  order = Order(**data)
+  db.session.add(order)
+  db.session.commit()
+  return jsonify(order.serialize()), 201
